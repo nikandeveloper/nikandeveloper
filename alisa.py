a7eaf8f5fd85a62520f4824fdee12e0554c9339c
nikandeveloper <93449288+nikandeveloper@users.noreply.github.com>
2023-08-24 21:36:52 +0330

    Update alisa.py

--- alisa.py
+++ alisa.py
@@ -1,23 +1,33 @@
 import pygame
+from pygame.locals import *
 import sys
 
 pygame.init()
 main = pygame.display.set_mode((627, 700))
-gravity = 7
+gravity = 15
 move = 0
 x = 100
-y = 510
+y = 100
 xchange = 0
 img2 = pygame.image.load("ao.png")
 img3 = pygame.image.load("alisan.png")
+reeect = img3.get_width()
+reect = img3.get_height()
+reeect2 = img2.get_width()
+reect2 = img2.get_height()
+pygame.draw.rect()
 rect = img3.get_rect()
 rect.center = (x, y)
+propertis = Rect(x, y, reect, reeect)
+propertis2 = Rect(0, 600, reect2, reeect2)
 pygame.display.set_caption("fmg")
 clock = pygame.time.Clock()
+collide = pygame.Rect.colliderect(propertis, propertis2)
 while True:
     clock.tick(60)
     x += xchange
-    y -= gravity
+    if not collide:
+      y += gravity
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -35,6 +45,8 @@ while True:
                 xchange = 0
     color = (255, 255, 255)
     main.fill(color)
-    main.blit(img2, (q, y))
+    pygame.draw.rect(main, "BLUE", propertis)
+    pygame.draw.rect(main, "BLUE", propertis2)
+    main.blit(img2, (x, y))
     main.blit(img3, (0, 600))
     pygame.display.update()
