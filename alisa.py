a03ea0c203740a87badd23738d1ca209be938008
mmdaliho <142893919+mmdaliho@users.noreply.github.com>
2023-09-03 14:27:46 +0330

    Add files via upload

--- alisa.py
+++ alisa.py
@@ -1,52 +1,38 @@
 import pygame
-from pygame.locals import *
 import sys
-
 pygame.init()
-main = pygame.display.set_mode((627, 700))
-gravity = 15
-move = 0
+x_display = 550
+y_display = 610
+main = pygame.display.set_mode((x_display, y_display))
+whate_c = (255,255,255)
+main_img0 = pygame.transform.scale2x(pygame.image.load("S0.png"))
+main_img1 = pygame.transform.scale2x(pygame.image.load("S1.png"))
+main_img2 = pygame.transform.scale2x(pygame.image.load("S2.png"))
+main_imgF = pygame.image.load("alisan 1.png")
+x_change = 0
 x = 100
-y = 100
-xchange = 0
-img2 = pygame.image.load("ao.png")
-img3 = pygame.image.load("alisan.png")
-reeect = img3.get_width()
-reect = img3.get_height()
-reeect2 = img2.get_width()
-reect2 = img2.get_height()
-pygame.draw.rect()
-rect = img3.get_rect()
-rect.center = (x, y)
-propertis = Rect(x, y, reect, reeect)
-propertis2 = Rect(0, 600, reect2, reeect2)
-pygame.display.set_caption("fmg")
+xF = 0
 clock = pygame.time.Clock()
-collide = pygame.Rect.colliderect(propertis, propertis2)
 while True:
-    clock.tick(60)
-    x += xchange
-    if not collide:
-      y += gravity
+    x += x_change
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-              ...
-        if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                xchange = -1
+                x_change = -3
             elif event.key == pygame.K_d:
-                xchange = 1
+                x_change = 3
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_d:
-                xchange = 0
-    color = (255, 255, 255)
-    main.fill(color)
-    pygame.draw.rect(main, "BLUE", propertis)
-    pygame.draw.rect(main, "BLUE", propertis2)
-    main.blit(img2, (x, y))
-    main.blit(img3, (0, 600))
+                x_change = 0
     pygame.display.update()
+    main.fill(whate_c)
+    xF -= 1
+    main.blit(main_img0, (x, 330))
+    main.blit(main_imgF, (xF, 510))
+    main.blit(main_imgF, (xF + 550, 510))
+    if xF <= -550 :
+        xF = 0
+clock.tick(60)
